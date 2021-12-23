40aabe792ae4feedeb08c615c307dbd8692d7721
sfann <sfann@logitech.com>
2021-12-23 14:54:36 +0800

    Update CameraPreviewTest source code.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -13,22 +15,33 @@ import android.hardware.camera2.CaptureResult;
 import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.OutputConfiguration;
 import android.hardware.camera2.params.SessionConfiguration;
+import android.media.Image;
+import android.media.ImageReader;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images;
 import android.util.Log;
 import android.view.Surface;
 import android.view.TextureView;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
 
-public class MainActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener {
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "fann";
 
     private CameraManager mCameraManager;
     private CameraDevice mCameraDevice;
-    private CaptureRequest.Builder mCaptureRequest;
+    private CaptureRequest.Builder mCaptureRequestBuilder;
+//    private ImageReader mImageReader;
 
     private TextureView mTextureView;
     private Handler mBackgroundHandler;
@@ -41,66 +54,71 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
         mCameraManager = getSystemService(CameraManager.class);
 
         mTextureView = new TextureView(this);
-        mTextureView.setSurfaceTextureListener(this);
+        mTextureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surface, int width, int height) {
+                Log.d(TAG, "onSurfaceTextureAvailable: " + width + ", " + height);
+                openCamera(width, height);
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surface, int width, int height) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(@NonNull SurfaceTexture surface) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(@NonNull SurfaceTexture surface) {
+
+            }
+        });
         setContentView(mTextureView);
 
         HandlerThread mBackgroundThread = new HandlerThread("CameraBackground");
         mBackgroundThread.start();
         mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+
+//        mImageReader = ImageReader.newInstance(100, 100,
+//                ImageFormat.JPEG, 1);
+//        mImageReader.setOnImageAvailableListener(mOnImageAvailableListener, mBackgroundHandler);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
+    private ImageReader.OnImageAvailableListener mOnImageAvailableListener = reader -> {
+        Image image = reader.acquireLatestImage();
+        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+        byte[] bytes = new byte[buffer.remaining()];
+        buffer.get(bytes);
 
-    @Override
-    public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surface, int width, int height) {
-        Log.d("fann", "onSurfaceTextureAvailable: " + width + ", " + height);
-        openCamera(width, height);
-    }
+        OutputStream output = null;
+        try {
+            ContentValues values = new ContentValues();
+            values.put(Images.Media.TITLE, "title");
+            values.put(Images.Media.DISPLAY_NAME, "name");
+            values.put(Images.Media.DESCRIPTION, "description");
+            values.put(Images.Media.MIME_TYPE, "image/jpeg");
+            // Add the date meta data to ensure the image is added at the front of the gallery
+            values.put(Images.Media.DATE_ADDED, System.currentTimeMillis());
+            values.put(Images.Media.DATE_TAKEN, System.currentTimeMillis());
 
-    @Override
-    public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surface, int width, int height) { }
-
-    @Override
-    public boolean onSurfaceTextureDestroyed(@NonNull SurfaceTexture surface) {
-        return true;
-    }
-
-    @Override
-    public void onSurfaceTextureUpdated(@NonNull SurfaceTexture surface) { }
-
-    private CameraCaptureSession.CaptureCallback mCaptureCallback
-            = new CameraCaptureSession.CaptureCallback() {
-        @Override
-        public void onCaptureProgressed(CameraCaptureSession session,
-                                        CaptureRequest request,
-                                        CaptureResult partialResult) {
-        }
-
-        @Override
-        public void onCaptureCompleted(CameraCaptureSession session,
-                                       CaptureRequest request,
-                                       TotalCaptureResult result) {
-        }
-    };
-
-    CameraCaptureSession.StateCallback mStateCallback = new CameraCaptureSession.StateCallback() {
-        @Override
-        public void onConfigured(@NonNull CameraCaptureSession session) {
-            Log.d("fann", "onConfigured!!");
-            try {
-                session.setRepeatingRequest(mCaptureRequest.build(),
-                        mCaptureCallback, mBackgroundHandler);
-            } catch (Exception e) {
-                Log.e("fann", "onConfigured Exception: " + e);
+            Uri fileUri = getContentResolver().insert(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            output = getContentResolver().openOutputStream(fileUri);
+            output.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            image.close();
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        }
-
-        @Override
-        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-            Log.d("fann", "onConfigureFailed");
         }
     };
 
@@ -109,45 +127,70 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
             mCameraManager.openCamera("0", new CameraDevice.StateCallback() {
                 @Override
                 public void onOpened(@NonNull CameraDevice camera) {
-                    Log.d("fann", "onOpened");
+                    Log.d(TAG, "onOpened");
                     mCameraDevice = camera;
-                    createCameraPreviewSession();
+                    createCameraSession();
                 }
 
                 @Override
                 public void onDisconnected(@NonNull CameraDevice camera) {
-                    Log.d("fann", "onDisconnected");
+                    Log.d(TAG, "onDisconnected");
                     mCameraDevice.close();
                     mCameraDevice = null;
                 }
 
                 @Override
                 public void onError(@NonNull CameraDevice camera, int error) {
-                    Log.d("fann", "onError");
+                    Log.d(TAG, "onError");
                     mCameraDevice.close();
                     mCameraDevice = null;
                 }
             }, null);
         } catch (Exception e) {
-            Log.d("fann", "openCamera Exception: " + e);
+            Log.d(TAG, "openCamera Exception: " + e);
         }
     }
 
-    private void createCameraPreviewSession() {
+    private void createCameraSession() {
         try {
             SurfaceTexture surfaceTexture = mTextureView.getSurfaceTexture();
             Surface surface = new Surface(surfaceTexture);
-            mCaptureRequest = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            mCaptureRequest.addTarget(surface);
+            mCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            mCaptureRequestBuilder.addTarget(surface);
+//            mCaptureRequestBuilder.addTarget(mImageReader.getSurface());
 
             List<OutputConfiguration> outConfigurations = new ArrayList<>();
             outConfigurations.add(new OutputConfiguration(surface));
+//            outConfigurations.add(new OutputConfiguration((mImageReader.getSurface())));
             SessionConfiguration sessionConfig = new SessionConfiguration(
-                    SessionConfiguration.SESSION_REGULAR, outConfigurations, Executors.newSingleThreadExecutor(), mStateCallback);
+                    SessionConfiguration.SESSION_REGULAR, outConfigurations,
+                    Executors.newSingleThreadExecutor(),
+                    new CameraCaptureSession.StateCallback() {
+
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession session) {
+                    Log.d(TAG, "onConfigured");
+                    try {
+                        session.setRepeatingRequest(mCaptureRequestBuilder.build(),
+                                null, mBackgroundHandler);
+
+//                session.stopRepeating();
+//                session.abortCaptures();
+//                session.capture(mCaptureRequestBuilder.build(), null, mBackgroundHandler);
+                    } catch (Exception e) {
+                        Log.e(TAG, "onConfigured Exception: " + e);
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+                }
+            });
 
             mCameraDevice.createCaptureSession(sessionConfig);
         } catch (CameraAccessException e) {
-            Log.e("fann", "CameraAccessException: " + e);
+            Log.e(TAG, "CameraAccessException: " + e);
         }
     }
 }
